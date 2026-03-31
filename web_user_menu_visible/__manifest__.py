d9293823ad701dffa4dfef419da740c85614a928
mehedihsn007 <mehedi.hsn@brainstation-23.com>
2026-03-31 21:03:24 +0600

    [ADD] web_user_menu_visible: added new app for 19v

--- web_user_menu_visible/__manifest__.py
+++ web_user_menu_visible/__manifest__.py
@@ -6,19 +6,21 @@
         regardless of debug mode. By default, Odoo only shows the username when in debug mode,
         but this module ensures it's always visible.
     """,
-    "author": "Brain Station 23",
+    "author": "Brain Station 23 PLC.",
     "website": "https://brainstation-23.com",
     "category": "Web",
     "version": "19.0.1.0.0",
     "depends": ["web"],
     "data": [],
+    "images": [
+        "static/description/banner.gif",
+    ],
     "assets": {
         "web.assets_backend": [
             "web_user_menu_visible/static/src/xml/user_menu.xml",
         ],
     },
     "license": "LGPL-3",
-    "support": "erp23@brainstation-23.com",
     "auto_install": False,
     "installable": True,
 }
