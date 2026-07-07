446547a520969c3eacd64e38a361d4a9f09ef959
Kamrul <kamrul.hasan@brainstation-23.com>
2026-07-07 19:18:12 +0600

    [FIX] author

--- theme_kingdom/__manifest__.py
+++ theme_kingdom/__manifest__.py
@@ -50,7 +50,7 @@ Sales → Pricelists for offer pricing. eCommerce → Promotions for cart discou
     ''',
     'category': 'Theme/eCommerce',
     'version': '19.0.1.0',
-    'author': 'ERP 23',
+    'author': 'ERP23',
     'company': 'nopCommerce, Brainstation 23 PLC',
     'maintainer': 'ERP 23',
     'website': 'https://www.erp-23.com',
